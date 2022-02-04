e725d2c11cf21cd2b98b59d51818b52680b3adbc
milickij <milickij@gmail.com>
2022-02-04 10:18:58 +0300

    Подключаем локализацию компонентов AntDesign

---
+++ src/views/Index.vue
@@ -0,0 +1,27 @@
+<template>
+  <a-config-provider :locale="locale">
+    <a-row>
+      <a-col :span="22" :offset="1">
+        <h1>Grid</h1>
+        <grid
+      /></a-col>
+    </a-row>
+  </a-config-provider>
+</template>
+
+<script>
+import ruRU from "ant-design-vue/lib/locale-provider/ru_RU";
+import Grid from "@/components/Grid";
+
+export default {
+  components: {
+    Grid,
+  },
+  data() {
+    return {
+      locale: ruRU,
+    };
+  },
+};
+</script>
+
