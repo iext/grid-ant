e49487c488afb29f6a90739d8bb7e20bedbfd1b0
milickij <milickij@gmail.com>
2022-02-04 10:14:10 +0300

    Подключаем vuex, router, antdesign

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import Vue from "vue";
+import App from "./App";
+import router from "@/router";
+import store from "@/store";
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/antd.css";
+
+Vue.use(Antd);
+
+Vue.config.productionTip = true;
+
+new Vue({
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
